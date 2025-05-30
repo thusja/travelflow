b33f42cab0844a44f6d165736470e64b67094a7f
thusja <wjdxo9292@naver.com>
2025-05-30 11:45:59 +0900

    db password fix

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "thus97",
+  password: "your password",
   database: "travel_flow",
   waitForConnections: true,
   connectionLimit: 10,
